e42691bf35842d1feb46e9d28c58a2e69cb83d31
Vinaybhai Chavada <vinaybhai.chavada@fau.de>
2023-11-26 23:17:20 +0100

    Test Case

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -5,6 +5,9 @@ KAGGLE_JSON_PATH=".~/kaggle/kaggle.json"
 KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
 export KAGGLE_CONFIG_DIR
 
+export KAGGLE_USERNAME="${{ secrets.KAGGLE_USERNAME }}"
+export KAGGLE_KEY="${{ secrets.KAGGLE_KEY }}"
+
 pip install --upgrade pip
 pip install -r ./project/requirements.txt
 python project/pipeline.py
