3f30884bdf8c60abe7858d562c2f3cbb11553946
Vinaybhai Chavada <vinaybhai.chavada@fau.de>
2023-11-26 23:35:06 +0100

    Test Case

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Export kaggle.json to os env for Kaggle authentication
+KAGGLE_JSON_PATH="./project/kaggle.json"
+KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
+export KAGGLE_CONFIG_DIR
+
 pip install --upgrade pip
 pip install -r ./project/requirements.txt
 python3 project/pipeline.py
