fa9a2b5ec53f3abd4bc267e7e6f81d229ea05fab
Vinaybhai Chavada <vinaybhai.chavada@fau.de>
2023-11-28 21:57:29 +0100

    Exercise 2

--- project/misc/function.py
+++ project/misc/function.py
@@ -2,6 +2,8 @@ import shutil
 import sqlite3
 import zipfile
 import pandas as pd
+import time
+from typing import Callable, Any
 
 
 def extract_and_move_co2_data(old_name: str, new_name: str):
@@ -9,13 +11,34 @@ def extract_and_move_co2_data(old_name: str, new_name: str):
     with zipfile.ZipFile(new_name, 'r') as zip_ref:
         zip_ref.extractall('co2_emission')
 
+
 def load_and_fill_missing(file_path, nan_fill_value=0, encoding='latin-1'):
     data = pd.read_csv(file_path, encoding=encoding)
     data.fillna(nan_fill_value, inplace=True)
     return data
+
 
 def save_to_csv_and_sql(data, csv_path, sql_path, table_name):
     data.to_csv(csv_path, index=False)
     conn = sqlite3.connect(sql_path)
     data.to_sql(table_name, conn, index=False, if_exists='replace')
     conn.close()
+
+
+def extract_csv_from_url(url: str, max_tries: int = 5, sec_wait_before_retry: float = 5) -> pd.DataFrame:
+    df = None
+    for i in range(1, max_tries + 1):
+        try:
+            df = pd.read_csv(url, sep=';', decimal=',')
+            break
+        except:
+            print(f'Couldn\'t extract csv from given url! (Try {i}/{max_tries})')
+            if i < max_tries: time.sleep(sec_wait_before_retry)
+    if df is None:
+        raise Exception(f'Failed to extract csv from given url {url}')
+    return df
+
+
+def drop_invalid_col(df: pd.DataFrame, column: str, valid: Callable[[Any], bool]) -> pd.DataFrame:
+    df = df.loc[df[column].apply(valid)]
+    return df
--- project/pipeline.py
+++ project/pipeline.py
@@ -1,19 +1,20 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
 from misc.function import extract_and_move_co2_data, load_and_fill_missing, save_to_csv_and_sql
+from misc.constant import DATA_SET_1, DATA_SET_1_FILE_NAME, DATA_SET_2, DATA_SET_2_FILE_NAME
 
 if __name__ == "__main__":
     api = KaggleApi()
     api.authenticate()
-    
+
     # Download and extract CO2 emission dataset
-    api.dataset_download_file('thedevastator/global-fossil-co2-emissions-by-country-2002-2022/data','GCB2022v27_MtCO2_flat.csv')
+    api.dataset_download_file(DATA_SET_1, DATA_SET_1_FILE_NAME)
     extract_and_move_co2_data('GCB2022v27_MtCO2_flat.csv.zip', 'co2_emission.zip')
-    co2_data = load_and_fill_missing('co2_emission/GCB2022v27_MtCO2_flat.csv')
-    
+    co2_data = load_and_fill_missing('co2_emission/' + DATA_SET_1_FILE_NAME)
+
     # Download and preprocess world population dataset
-    api.dataset_download_file('iamsouravbanerjee/world-population-dataset','world_population.csv')
-    population_data = load_and_fill_missing('world_population.csv', nan_fill_value=0)
-    
+    api.dataset_download_file(DATA_SET_2, DATA_SET_2_FILE_NAME)
+    population_data = load_and_fill_missing(DATA_SET_2_FILE_NAME, nan_fill_value=0)
+
     # Save datasets to CSV and SQLite
-    save_to_csv_and_sql(co2_data, 'data/GCB2022v27_MtCO2_flat.csv', 'data/co2_emission_test1.db', 'co2_emission_data')
-    save_to_csv_and_sql(population_data, 'data/world_population.csv', 'data/population_data.db', 'population_data')
+    save_to_csv_and_sql(co2_data, 'data/' + DATA_SET_1_FILE_NAME, 'data/co2_emission_test1.db', 'co2_emission_data')
+    save_to_csv_and_sql(population_data, 'data/' + DATA_SET_2_FILE_NAME, 'data/population_data.db', 'population_data')
