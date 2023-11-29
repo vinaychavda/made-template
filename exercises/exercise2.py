6dd45d8dfa0e83866ae884e92db4f195c92269f4
Vinaybhai Chavada <vinaybhai.chavada@fau.de>
2023-11-29 13:25:57 +0100

    Exercise 2

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -13,16 +13,12 @@ def extract_csv_from_url(url: str, max_attempts: int = 5, wait_time_before_retry
             dataframe = pd.read_csv(url, sep=';', decimal=',')
             break
         except pd.errors.EmptyDataError:
-            print(f'The CSV at the provided URL is empty! (Attempt {attempt}/{max_attempts})')
+            print(f'CSV at the provided URL is empty! (Attempt {attempt}/{max_attempts})')
             break
-        except pd.errors.ParserError:
-            print(f'Failed to parse the CSV from the provided URL! (Attempt {attempt}/{max_attempts})')
-        except pd.errors.HTTPError as e:
-            print(f'HTTPError: {e}. (Attempt {attempt}/{max_attempts})')
-        except pd.errors.RequestException as e:
-            print(f'RequestException: {e}. (Attempt {attempt}/{max_attempts})')
+        except (pd.errors.ParserError, pd.errors.HTTPError, pd.errors.RequestException) as e:
+            print(f'Error during extraction: {e}. (Attempt {attempt}/{max_attempts})')
         except Exception as e:
-            print(f'An unexpected error occurred: {e}. (Attempt {attempt}/{max_attempts})')
+            print(f'Unexpected error: {e}. (Attempt {attempt}/{max_attempts})')
 
         if attempt < max_attempts:
             time.sleep(wait_time_before_retry)
@@ -35,24 +31,23 @@ def extract_csv_from_url(url: str, max_attempts: int = 5, wait_time_before_retry
 
 def drop_invalid_col(df: pd.DataFrame, column: str, valid: Callable[[Any], bool]) -> pd.DataFrame:
     try:
-        # Keep only rows where the specified column satisfies the validation function
         df = df.loc[df[column].apply(valid)]
     except KeyError:
         raise KeyError(f'The specified column "{column}" does not exist in the DataFrame.')
     except Exception as e:
-        raise Exception(f'An unexpected error occurred while dropping invalid rows: {e}')
+        raise Exception(f'Error while dropping invalid rows: {e}')
 
     return df
 
 
 if __name__ == '__main__':
-    # Url to csv file
+    # URL to CSV file
     DATA_URL = 'https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV'
 
-    # Extract dataframe from csv (with retries)
+    # Extract dataframe from CSV (with retries)
     df = extract_csv_from_url(DATA_URL)
 
-    # Drop the Status column
+    # Drop the "Status" column
     df = df.drop(columns=['Status'])
 
     # Drop rows with invalid values
@@ -62,7 +57,7 @@ if __name__ == '__main__':
     df = drop_invalid_col(df, 'Breite', lambda x: -90 < x < 90)
     df = drop_invalid_col(df, 'IFOPT', lambda x: re.match('^..:[0-9]+:[0-9]+(:[0-9]+)?$', x) is not None)
 
-    # Load dataframe into sqlite database, with matching datatypes
+    # Load dataframe into SQLite database, with matching data types
     df.to_sql('trainstops', 'sqlite:///trainstops.sqlite', if_exists='replace', index=False, dtype={
         "EVA_NR": sqlalchemy.BIGINT,
         "DS100": sqlalchemy.TEXT,
