d6c8db19614b1e7e9696d14db73fcdec740cd404
Vinay <vinaybhaichavda@faustud-010-021-019-080.pool.uni-erlangen.de>
2024-03-21 18:47:37 +0100

    Pipeline issue fixed

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pip3 install --upgrade pip
-pip3 install -r requirements.txt
-python3 pipeline.py
-python3 test_project.py
+pip3 install -r ./requirements.txt
+python3 ./pipeline.py
+python3 ./test_project.py
